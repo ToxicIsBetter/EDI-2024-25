0c3f96bc973fe6a171a92c6650a4aa3b071ea2dd
Hard Joshi <hardjoshi.dev@gmail.com>
2025-02-25 11:51:39 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Toggle navbar style on scroll
 window.addEventListener("scroll", function () {
-    const navbar = document.querySelector("nav");
-    if (window.scrollY > 50) {
-        navbar.classList.add("scrolled");
-    } else {
-        navbar.classList.remove("scrolled");
-    }
+  const navbar = document.querySelector("nav");
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
 });
 
 // Mobile menu toggle functionality
@@ -13,7 +13,7 @@ const menuToggle = document.querySelector(".menu-toggle");
 const navUl = document.querySelector("nav ul");
 
 if (menuToggle) {
-    menuToggle.addEventListener("click", () => {
-        navUl.classList.toggle("active");
-    });
+  menuToggle.addEventListener("click", () => {
+    navUl.classList.toggle("active");
+  });
 }
